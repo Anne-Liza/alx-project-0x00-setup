d26cb80dbf6df15ca0a7b9305b22804c4cf5d165
Anne Liza Wangui <annewangui210@gmail.com>
2025-06-01 23:55:21 +0300

    edits landing.tsx

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -1,12 +1,19 @@
 import Card from "@/components/Card"
+import Button from "@/components/Button";
+
 
 const Landing: React.FC = () => {
     return (
-        <div>
+        <div className="p-8">
             <h1 className=" text-x1 font-extralight">Landing Page</h1>
-            <Card />
-            <Card />
-            <Card />
+            {/* Small Button - rounded-sm */}
+            <Button title="Small Button" styles="px-4 py-2 text-sm rounded-sm" />
+
+            {/* Medium Button - rounded-md */}
+            <Button title="Medium Button" styles="px-6 py-3 text-base rounded-md" />
+
+            {/* Large Button - rounded-full */}
+            <Button title="Large Button" styles="px-8 py-4 text-lg rounded-full" />
         </div>
     )
 }
